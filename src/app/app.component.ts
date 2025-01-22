abaa1e9dfe9ce45312f54b677fe30982181696dc
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-22 11:59:59 -0500

    configure basic get of database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DoctorAppFrontend';
+  titulo:string = 'Bienvenido a la APP';
+  usuarios:any;
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:5165/api/Usuario').subscribe
+    ({
+      next: (response) => {
+        this.usuarios = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('Complete');
+      }
+    });
+  }
+
 }
